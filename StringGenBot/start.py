fbaa6c3a6a1a1eada7f183a5ac0aedb379c31bfc
xtroo11 <123382578+xtroo11@users.noreply.github.com>
2023-05-29 00:57:15 +0300

    Add files via upload

---
+++ StringGenBot/start.py
@@ -0,0 +1,40 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+
+from config import OWNER_ID
+
+
+def filter(cmd: str):
+    return filters.private & filters.incoming & filters.command(cmd)
+
+@Client.on_message(filter("start"))
+async def start(bot: Client, msg: Message):
+    me2 = (await bot.get_me()).mention
+    await bot.send_message(
+        chat_id=msg.chat.id,
+        text=f"""¤¦ اهلا بـك عزيـزي 
+
+¤¦ يمكنك استـخـراج التالـي
+
+¤¦ تيرمڪس تليثون للحسابات
+
+¤¦ تيرمـكـس تليثون للبوتـات
+
+¤¦ بايـروجـرام مـيوزك للحسابات
+
+¤¦ بايـروجـرام مـيوزك للبوتات
+
+¤¦ تم انشاء البوت بواسطة [ㅤ𓏺 ժᥱ᥎ ᥉ᥲ️ꪔᎥٍᖇ . 🕷 ˼](https://t.me/DEV_SAMIR)""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="إضغط لبدا استخراج الكود", callback_data="generate")
+                ],
+                [
+                    InlineKeyboardButton("️sᴏᴜʀᴄᴇ ", url="https://t.me/XTRRO"),
+                    InlineKeyboardButton(" ᴅᴇᴠᴇ", user_id=OWNER_ID)
+                ]
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
